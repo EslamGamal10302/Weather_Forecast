e75e60d85475c665207dbf218581c5c87a7c68f8
eslam <eslam10302@gmail.com>
2023-03-19 02:25:48 +0200

    FirstVersionOfUi

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.weatherforecast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navigationView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigationView = findViewById(R.id.navigationView)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navController = findNavController(R.id.fragmentNavHost)
+        val actionBar = supportActionBar
+        actionBar?.setHomeAsUpIndicator(R.drawable.menu)
+        actionBar?.setDisplayShowHomeEnabled(true)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+       // appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+        NavigationUI.setupWithNavController(navigationView,navController)
+      //  setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
